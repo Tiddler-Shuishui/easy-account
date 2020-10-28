d2890f5fb3283a07e412742ed94bb863832d36eb
hugh <270222838@qq.com>
2020-10-28 22:57:10 +0800

    封装出一个 model

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,6 +1,5 @@
 <template>
   <Layout class-prefix="layout">
-    {{ record }}
     <NumberPad :value.sync="record.amount" @submit="saveRecord"/>
     <Types :value.sync="record.type"/>
     <Notes @update:value="onUpdateNotes"/>
@@ -15,9 +14,12 @@ import Types from '@/components/Money/Types.vue';
 import Notes from '@/components/Money/Notes.vue';
 import Tags from '@/components/Money/Tags.vue';
 import {Component, Watch} from 'vue-property-decorator';
+import model from '@/model';
+
+const recordList = model.fetch();
 
 const version: string = window.localStorage.getItem('version') || '0';
-const recordList: Record[] = JSON.parse(window.localStorage.getItem('recordList')||'[]');
+// const recordList: Record[] = JSON.parse(window.localStorage.getItem('recordList')||'[]');
 if (version === '0.0.1'){
   // 数据库升级，数据迁移
   recordList.forEach(record =>{
@@ -28,21 +30,14 @@ if (version === '0.0.1'){
 }
 window.localStorage.setItem('version','0.0.2')
 
-type Record = {
-  tags: string[];
-  notes: string;
-  type: string;
-  amount: number;
-  createdAt?: Date;
 
-}
 @Component({
   components: {Tags, Notes, Types, NumberPad}
 })
 export default class Money extends Vue {
   tags = ['衣', '食', '住', '行'];
-  record: Record = {tags: [], notes: '', type: '-', amount: 0};
-  recordList: Record[] = recordList;
+  record: RecordItem = {tags: [], notes: '', type: '-', amount: 0};
+  recordList: RecordItem[] = recordList;
 
   onUpdateTags(value: string[]) {
     this.record.tags = value;
@@ -53,14 +48,14 @@ export default class Money extends Vue {
   }
 
   saveRecord() {
-    const record2: Record = JSON.parse(JSON.stringify(this.record));
+    const record2: RecordItem = model.clone(this.record);
     record2.createdAt = new Date();
     this.recordList.push(record2);
   }
 
   @Watch('recordList')
   onRecordListChange() {
-    window.localStorage.setItem('recordList', JSON.stringify((this.recordList)));
+    model.save(this.recordList);
   }
 
 }
