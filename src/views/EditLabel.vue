4a43205115cbcf91b659cda1889aee25c5edb958
hugh <270222838@qq.com>
2020-10-29 19:06:42 +0800

    可以编辑和删除标签

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -1,15 +1,19 @@
 <template>
   <Layout>
     <div class="navBar">
-      <Icon class="leftIcon" name="left"/>
+      <Icon class="leftIcon" name="left" @click="goBack"/>
       <span class="title">编辑标签</span>
       <span class="rightIcon"></span>
     </div>
     <div class="form-wrapper">
-    <FormItem :value="tag.name" field-name="标签名" placeholder="请输入标签名"/>
+      <FormItem
+        :value="tag.name"
+        @update:value="updateTag"
+        field-name="标签名"
+        placeholder="请输入标签名"/>
     </div>
     <div class="button-wrapper">
-      <Button>删除标签</Button>
+      <Button @click="remove">删除标签</Button>
     </div>
 
   </Layout>
@@ -21,11 +25,13 @@ import {Component} from 'vue-property-decorator';
 import tagListModel from '@/models/tagListModel';
 import FormItem from '@/components/Money/FormItem.vue';
 import Button from '@/components/Button.vue';
+
 @Component({
-  components: {Button,FormItem}
+  components: {Button, FormItem}
 })
 export default class EditLabel extends Vue {
-  tag?: { id: string; name: string } = undefined
+  tag?: { id: string; name: string } = undefined;
+
   created() {
     const id = this.$route.params.id;
     tagListModel.fetch();
@@ -37,39 +43,59 @@ export default class EditLabel extends Vue {
       this.$router.replace('/404');
     }
   }
+
+  updateTag(name: string) {
+    if (this.tag) {
+      tagListModel.update(this.tag.id, name);
+    }
+
+  }
+
+  remove() {
+    if (this.tag) {
+      tagListModel.update(this.tag.id, name);
+    }
+  }
+  goBack(){
+    this.$router.back()
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-  .navBar{
-    text-align: center;
-    font-size: 16px;
-    padding: 12px 16px;
-    background: white;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    > .title{
+.navBar {
+  text-align: center;
+  font-size: 16px;
+  padding: 12px 16px;
+  background: white;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 
-    }
-    > .leftIcon{
-      width: 24px;
-      height: 24px;
-    }
-    > .rightIcon{
-      width: 24px;
-      height: 24px;
-    }
+  > .title {
 
   }
 
-  .form-wrapper{
-    background: white;
-    margin-top: 8px;
+  > .leftIcon {
+    width: 24px;
+    height: 24px;
   }
-  .button-wrapper{
-    text-align: center;
-    padding: 16px;
-    margin-top: 44-16px;
+
+  > .rightIcon {
+    width: 24px;
+    height: 24px;
   }
+
+}
+
+.form-wrapper {
+  background: white;
+  margin-top: 8px;
+}
+
+.button-wrapper {
+  text-align: center;
+  padding: 16px;
+  margin-top: 44-16px;
+}
 </style>
