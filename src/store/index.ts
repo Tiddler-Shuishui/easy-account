c6b1168d3f1f3c0e24bc3f355f395353f6c9edc6
hugh <270222838@qq.com>
2020-11-07 01:22:22 +0800

    修复刷新变成 404 的 bug

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import clone from '@/lib/clone';
 import createId from '@/lib/createId';
+import router from '@/router';
 
 Vue.use(Vuex);
 type RootState = {
@@ -16,24 +17,55 @@ const store = new Vuex.Store({
     currentTag: undefined
   } as RootState,
   mutations: {
-    fetchRecords(state) {
-      state.recordList = JSON.parse(window.localStorage.getItem('recordList') || '[]') as RecordItem[]
+    removeTag(state, id: string) {
+      let index = -1;
+      for (let i = 0; i < state.tagList.length; i++) {
+        if (state.tagList[i].id === id) {
+          index = i;
+          break;
+        }
+      }
+      if (index >= 0) {
+        state.tagList.splice(index, 1);
+        store.commit('saveTags');
+        router.back();
+      } else {
+        window.alert('删除失败');
+      }
     },
-    createRecord(state, record){
+    updateTag(state, payload: { id: string; name: string }) {
+      const {id, name} = payload;
+
+      const idList = state.tagList.map(item => item.id);
+      if (idList.indexOf(id) >= 0) {
+        const names = state.tagList.map(item => item.name);
+        if (names.indexOf(name) >= 0) {
+          window.alert('标签名重复了');
+        } else {
+          const tag = state.tagList.filter(item => item.id === id)[0];
+          tag.name = name;
+          store.commit('saveTags');
+        }
+      }
+    },
+    fetchRecords(state) {
+      state.recordList = JSON.parse(window.localStorage.getItem('recordList') || '[]') as RecordItem[];
+    },
+    createRecord(state, record) {
       const record2: RecordItem = clone(record);
       record2.createdAt = new Date();
       state.recordList.push(record2);
-      store.commit('saveRecords')
+      store.commit('saveRecords');
     },
     saveRecords(state) {
       window.localStorage.setItem('recordList',
         JSON.stringify(state.recordList));
     },
-    fetchTags(state){
+    fetchTags(state) {
       state.tagList = JSON.parse(window.localStorage
-        .getItem('tagList') || '[]')
+        .getItem('tagList') || '[]');
     },
-    createTag (state,name: string) {
+    createTag(state, name: string) {
 
       const names = state.tagList.map(item => item.name);
       if (names.indexOf(name) >= 0) {
@@ -41,15 +73,15 @@ const store = new Vuex.Store({
       }
       const id = createId().toString();
       state.tagList.push({id: id, name: name});
-      store.commit('saveTags')
+      store.commit('saveTags');
       window.alert('添加成功');
     },
     saveTags(state) {
       window.localStorage.setItem('tagList', JSON.stringify(state.tagList));
     },
-    setCurrentTag(state,id: string){
-      state.currentTag = state.tagList.filter(t => t.id === id)[0]
+    setCurrentTag(state, id: string) {
+      state.currentTag = state.tagList.filter(t => t.id === id)[0];
     }
   }
 });
-export default store
+export default store;
