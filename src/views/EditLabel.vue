eb93e30a381713954ef321a5bbc5b74ee7afb821
hugh <270222838@qq.com>
2020-11-06 20:58:16 +0800

    Money.vue 使用 Vuex

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -24,7 +24,6 @@ import Vue from 'vue';
 import {Component} from 'vue-property-decorator';
 import FormItem from '@/components/Money/FormItem.vue';
 import Button from '@/components/Button.vue';
-import store from '@/store/index2';
 
 @Component({
   components: {Button, FormItem}
@@ -33,7 +32,8 @@ export default class EditLabel extends Vue {
   tag?: Tag = undefined;
 
   created() {
-    this.tag = store.findTag(this.$route.params.id);
+    //TODO
+    // this.tag = store.findTag(this.$route.params.id);
     if (!this.tag) {
       this.$router.replace('/404');
     }
@@ -41,18 +41,21 @@ export default class EditLabel extends Vue {
 
   updateTag(name: string) {
     if (this.tag) {
-      store.updateTag(this.tag.id, name);
+      //TODO
+      //store.updateTag(this.tag.id, name);
     }
 
   }
 
   remove() {
     if (this.tag) {
-      if(store.removeTag(this.tag.id)){
-        this.$router.back()
-      } else {
-        window.alert('删除失败')
-      }
+      //TODO
+      return
+      // if(store.removeTag(this.tag.id)){
+      //   this.$router.back()
+      // } else {
+      //   window.alert('删除失败')
+      // }
     }
   }
   goBack(){
